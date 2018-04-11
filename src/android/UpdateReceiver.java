1a65a9c2838a3316e498b0ff095a6caab2d85b81
Michael McDuffee <mmcduffee@lyft.com>
2018-04-11 10:55:09 -0700

    fixing update receiver

--- src/android/UpdateReceiver.java
+++ src/android/UpdateReceiver.java
@@ -9,12 +9,12 @@ public class MyPackageReplacedEventReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             System.out.println("Kiosk application start after boot");
-            Intent newIntent = new Intent(context, KioskActivity.class);
+            Intent newIntent = new Intent(context, LockTaskActivity.class);
             newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(newIntent);
         } else if(intent.getAction().equals("android.intent.action.MY_PACKAGE_REPLACED")){
             System.out.println("Kiosk application restarting after upgrade");
-            Intent newIntent = new Intent(context, KioskActivity.class);
+            Intent newIntent = new Intent(context, LockTaskActivity.class);
             newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(newIntent);
         }
